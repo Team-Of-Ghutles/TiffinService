2cc5d129a933bab27f33f12ab06bca74d3c808d0
srikantviswanath <srikantviswanath8@gmail.com>
2017-04-26 19:29:27 -0400

    Added updateUserBalance function in index.js
    Updates credit/debit key under 'balances' node in Firebase upon a
    transaction write

--- TiffinService/functions/index.js
+++ TiffinService/functions/index.js
@@ -1,8 +1,24 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.updateUserBalance = functions.database.ref('Transactions/{userId}/{txnId}')
+	.onWrite( event => {
+		if (!event.data.exists()) {
+			return;
+		} else {
+			const balancePerUserRef = event.data.ref.root.child('Balances').child(event.params.userId)
+			var txnData = event.data.val();
+			var eventType = txnData['Type'];
+			var balanceType = eventType == 'ORDER' ? 'debit' : 'credit';
+			return balancePerUserRef.child(balanceType).transaction( current => {
+				if (!current) {
+					return txnData['Amount'];
+				} else {
+					return current + txnData['Amount'];
+				}
+			}).then(() => {
+				console.log('Balance node for ', event.params.userId, '/', balanceType, 'updated');
+			});
+		}
+	});
